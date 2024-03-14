0cda09e1c97b1757f163598016b7d84bec1fba2b
Kobe Li <likobe23@gmail.com>
2024-03-13 19:51:14 -0700

    Add string file io tests

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ build:
 	gcc -o main main.c array.c hashing.c string.c
 
 test:
+	rm -rf tests/bin;
 	mkdir tests/bin;
 	gcc -o tests/bin/test_array_int tests/test_array_int.c array.c;
 	gcc -o tests/bin/test_array_struct tests/test_array_struct.c array.c;
--- tests/test_string.c
+++ tests/test_string.c
@@ -3,7 +3,9 @@
 
 #include "../string.h"
 
-int test_string_assignment(char s[]) {
+int test_string_assignment() {
+    char s[] = "Hello World!";
+
     String* str = new_string(s);
 
     if (strlen(str->value) != str->length) {
@@ -23,7 +25,33 @@ int test_string_assignment(char s[]) {
     return 0;
 }
 
-int main() {
-    int test_result = test_string_assignment("Hello world!");
+int test_string_file_io() {
+    String* expected_value = new_string("Hello, Deep!");
+
+    FILE* fptr = fopen("test_strings", "wb");
+    write_string_to_file(expected_value, &fptr);
+    fclose(fptr);
+
+    fptr = fopen("test_strings", "rb");
+    String* read_value = read_string_from_file(&fptr);
+    fclose(fptr);
+
+    if (expected_value->length != read_value->length) {
+        printf("Unequal string lengths: Expected %lu, got %lu\n", expected_value->length, read_value->length);
+        return -1;
+    }
+
+    if (strcmp(expected_value->value, read_value->value) != 0) {
+        printf("Unequal strings: Expected %s, got %s", expected_value->value, read_value->value);
+        return -1;
+    }
+
     return 0;
+}
+
+int main() {
+    int test_result = 0;
+    test_result = test_string_assignment();
+    test_result = test_string_file_io();
+    return test_result;
 }
